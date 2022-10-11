a491a14adcd10fe54189f6b4e39291d1e9c00a5a
hisham-css <61122654+hisham-CSS@users.noreply.github.com>
2022-10-11 13:16:59 -0400

    Lab 2 - incomplete

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger Entered");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Trigger Exited");
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("Trigger Staying");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collider Entered");
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Collider Exited");
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("Collider Staying");
+    }
+}
