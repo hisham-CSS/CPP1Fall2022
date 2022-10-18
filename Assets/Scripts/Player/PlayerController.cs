1f48bd8727acb91822938dc94b016dd21417ced1
hisham-css <61122654+hisham-CSS@users.noreply.github.com>
2022-10-18 12:51:26 -0400

    Lab 3 - incomplete

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
+public class PlayerController : MonoBehaviour
+{
+    //components
+    Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer sr;
+
+    //movement stuff
+    public float speed;
+    public float jumpForce;
+
+    //groundcheck stuff
+    public bool isGrounded;
+    public Transform groundCheck;
+    public LayerMask isGroundLayer;
+    public float groundCheckRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+
+        if (speed <= 0)
+        {
+            speed = 6.0f;
+            Debug.Log("Speed Not Set - Default to 6");
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 300;
+            Debug.Log("Jump Force Not Set - Default to 300");
+        }
+
+        if (!groundCheck)
+        {
+            groundCheck = GameObject.FindGameObjectWithTag("GroundCheck").transform;
+            Debug.Log("Ground Check Not Set - Finding it Manually");
+        }
+
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.2f;
+            Debug.Log("Ground Check Radius Not Set - Default to 0.2");
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxisRaw("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+        rb.velocity = moveDirection;
+
+        anim.SetFloat("hInput", Mathf.Abs(hInput));
+        anim.SetBool("isGrounded", isGrounded);
+
+        //check for flipped and create some sort of algorithm to keep your sprite flipped properly.
+    }
+}
